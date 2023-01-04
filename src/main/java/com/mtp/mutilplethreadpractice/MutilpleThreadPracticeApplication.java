c43d2bb7820f4a9819a2e105882bc53c20bc0934
liangxiaole <xiaoleliang045@gmail.com>
2023-01-04 23:06:43 +0900

    first

---
+++ src/main/java/com/mtp/mutilplethreadpractice/MutilpleThreadPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.mtp.mutilplethreadpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MutilpleThreadPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MutilpleThreadPracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mtp/mutilplethreadpractice/MutilpleThreadPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mtp.mutilplethreadpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MutilpleThreadPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
